023d7ce5f419a337f48fa41a9c7b333aafc71ba3
Åsa Palholmen <asa@palholmen.se>
2019-01-23 17:48:51 +0100

    splitted up so reward is in another file.

---
+++ hatch.js
@@ -0,0 +1,117 @@
+var hatch = [{
+    color: 'red',
+    reward: 'Jackpott',
+    id: 1
+  },
+  {
+    color: 'purple',
+    reward: 'a Marwin Robot',
+    id: 2
+  },
+  {
+    color: 'blue',
+    reward: 'to the backside of the moon',
+    id: 3
+  },
+  {
+    color: 'yellow',
+    reward: 'a spacecraft',
+    id: 4
+  },
+  {
+    color: 'green',
+    reward: 'Matcha thee',
+    id: 5
+  },
+
+  {
+    color: 'red',
+    reward: 'monolopy money',
+    id: 6
+  },
+  {
+    color: 'yellow',
+    reward: 'a Beehive',
+    id:7
+  }, {
+    color: 'purple',
+    reward: 'a kitchenrobot',
+    id:8
+  },
+  {
+    color: 'green',
+    reward: 'red meat',
+    id:9
+  },
+  {
+    color: 'red',
+    reward: 'The heart of gold',
+    id: 10
+  },
+  {
+    color: 'red',
+    reward: 'a flying island',
+    id:11
+  },
+  {
+    color: 'blue',
+    reward: 'a Narnia Warddrobe',
+    id:12
+  },
+  {
+    color: 'yellow',
+    reward: 'Positive robotdoors',
+    id:13
+  },
+  {
+    color: 'green',
+    reward: 'soilent green',
+    id:14
+  },
+  {
+    color: 'blue',
+    reward: 'a weekend in Nangiala',
+    id:15
+  }, {
+    color: 'blue',
+    reward: 'the planet Europa',
+    id:16
+  }, {
+    color: 'purple',
+    reward: 'a castle',
+    id:17
+  }, {
+    color: 'yellow',
+    reward: 'a basebollbat with spikes',
+    id:18
+  }, {
+    color: 'green',
+    reward: 'Kattla',
+    id:19
+  }, {
+    color: 'purple',
+    reward: 'a lasergun',
+    id:20
+  }, {
+    color: 'blue',
+    reward: 'coconuts',
+    id:21
+  }, {
+    color: 'yellow',
+    reward: 'a skull',
+    id:22
+  }, {
+    color: 'green',
+    reward: 'holy water',
+    id:23
+  }, {
+    color: 'red',
+    reward: 'a Tesla',
+    id:24
+  }, {
+    color: 'purple',
+    reward: 'a Tesla coil',
+    id:25
+  }];
+
+export default hatch;
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import hatch from './hatch.js'
+
 var bingoNr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 var bingoGrid = bingoNr.sort(() => 0.5 - Math.random());
 
@@ -58,7 +60,6 @@ grid.addEventListener('click', event => {
     if (firstGuess == secondGuess) {
       console.log('match');
     }
-
   }
 
   previousTarget = clicked;
